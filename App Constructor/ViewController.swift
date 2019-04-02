10cfd6f7faf0bdb5010d74209dd685cecb598f1b
Student <student@c-20-4.local>
2019-04-02 16:00:28 +0300

    Initial Commit

---
+++ App Constructor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Constructor
+//
+//  Created by Student on 02/04/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
